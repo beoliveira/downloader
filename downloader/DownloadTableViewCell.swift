a64ae4b20dae31157ddeaabf7fbb1c2a9c6631cf
Andrew Tokarev <git@atkit.com>
2016-04-16 21:43:35 +0200

    Moved imageForMIMEType to separate extension of DownloadTableViewCell to separate this code from the main implementation

--- downloader/DownloadTableViewCell.swift
+++ downloader/DownloadTableViewCell.swift
@@ -21,35 +21,6 @@ class DownloadTableViewCell: UITableViewCell {
         // Initialization code
     }
     
-    //
-    // Returns an UIImage instance containing the icon for the
-    // corresponding MIME type string
-    //
-    func imageForMIMEType(mimeTypeString:String) -> UIImage! {
-        
-        switch mimeTypeString
-        {
-        case "audio/mpeg":     fallthrough
-        case "audio/x-mpeg-3": fallthrough
-        case "video/mpeg":     fallthrough
-        case "video/x-mpeg":
-            return UIImage(named: "mp3File")
-            
-        case "application/pdf":
-            return UIImage(named: "pdfFile")
-            
-        case "image/png":
-            return UIImage(named: "pngFile")
-            
-        case "image/jpeg": fallthrough
-        case "image/pjpeg":
-            return UIImage(named: "jpgFile")
-            
-        default:
-            return UIImage(named: "blankFile")
-        }
-    }
-
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
@@ -137,6 +108,37 @@ class DownloadTableViewCell: UITableViewCell {
         //
         NSNotificationCenter.defaultCenter().addObserverForName(download.startDateString, object: nil, queue: nil) { (notification) -> Void in
             self.setupInterfaceForProgressUpdate(notification.userInfo)
+        }
+    }
+}
+
+extension DownloadTableViewCell /* MIME Type -> Image */ {
+    //
+    // Returns an UIImage instance containing the icon for the
+    // corresponding MIME type string
+    //
+    func imageForMIMEType(mimeTypeString:String) -> UIImage! {
+        
+        switch mimeTypeString
+        {
+        case "audio/mpeg":     fallthrough
+        case "audio/x-mpeg-3": fallthrough
+        case "video/mpeg":     fallthrough
+        case "video/x-mpeg":
+            return UIImage(named: "mp3File")
+            
+        case "application/pdf":
+            return UIImage(named: "pdfFile")
+            
+        case "image/png":
+            return UIImage(named: "pngFile")
+            
+        case "image/jpeg": fallthrough
+        case "image/pjpeg":
+            return UIImage(named: "jpgFile")
+            
+        default:
+            return UIImage(named: "blankFile")
         }
     }
 }
